56e7f97208a93888e14c67fd2e7d42151939c743
CrispenGari <Crispengari@gmail.com>
2022-02-14 20:47:36 +0200

    updated

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default () => (
   <Head>
     <meta charSet="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Hand Written Digits</title>
     <link rel="shortcut icon" href="/mnist.png" type="image/x-icon" />
