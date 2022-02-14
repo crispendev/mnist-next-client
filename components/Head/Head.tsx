dc9996b420f531ccc8c88b33ae7942e8e58f4850
CrispenGari <Crispengari@gmail.com>
2022-02-14 20:38:47 +0200

    init-commit

---
+++ components/Head/Head.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default () => (
+  <Head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hand Written Digits</title>
+    <link rel="shortcut icon" href="/mnist.png" type="image/x-icon" />
+  </Head>
+);
---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+import Layout from "../components/Layout/Layout";
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
+export default App;
